ced162489117acf2091231afafa69c2e73bf5937
Giacomo Pessotto <giacomopesso@gmail.com>
2023-02-02 22:00:20 +0000

    added hint

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
       }
     }
     document.getElementById('choose-photo')!.style.display = "none"
+    document.getElementById('choose-photo2')!.style.display = "none"
     document.getElementById('photo-button')!.style.display = "none"
     document.getElementById('photo-button2')!.style.display = "flex"
     document.getElementById('form')!.style.display = "block"
@@ -89,7 +90,8 @@ const handleResult = async (result:any) => {
       </button>
     <div className=' bg-slate-500 h-72 mt-24 w-72 rounded-md text-center content-center'>
       <p id='choose-photo' className='text-white mt-4 font-serif font-medium text-xl'>Choose A Photo</p>
-      <button id='photo-button' className='bg-blue-500 duration-1000 shadow-md hover:bg-slate-200 fill-white hover:fill-black w-32 h-12 ml-20 mt-20 rounded-md flex justify-center items-center'>
+      <p id='choose-photo2' className='text-white mt-1 font-serif font-medium text-sm'>*Image Must Fill Square</p>
+      <button id='photo-button' className='bg-blue-500 duration-1000 shadow-md hover:bg-slate-200 fill-white hover:fill-black w-32 h-12 ml-20 mt-16 rounded-md flex justify-center items-center'>
       <input id='input' type="file" onChange={(e)=> convertFile(e.target.files)} className=" opacity-0 w-24 ml-0 absolute" />
       <svg xmlns="http://www.w3.org/2000/svg"  height="48" width="48"><path d="M10 42 6 18h36l-4 24Zm2.5-3h23l2.85-18H9.65ZM20 29h8q.65 0 1.075-.45.425-.45.425-1.05 0-.65-.425-1.075Q28.65 26 28 26h-8q-.6 0-1.05.425-.45.425-.45 1.075 0 .6.45 1.05.45.45 1.05.45Zm-8-14q-.6 0-1.05-.45-.45-.45-.45-1.05 0-.65.45-1.075Q11.4 12 12 12h24q.65 0 1.075.425.425.425.425 1.075 0 .6-.425 1.05Q36.65 15 36 15Zm4-6q-.6 0-1.05-.45-.45-.45-.45-1.05 0-.65.45-1.075Q15.4 6 16 6h16q.65 0 1.075.425.425.425.425 1.075 0 .6-.425 1.05Q32.65 9 32 9Zm-3.5 30h23Z"/></svg>
       </button>
